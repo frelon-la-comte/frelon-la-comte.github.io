e4ec07270ce452bf161553c6b5d45c617ffcc3ed
frelon-la-comte <lacomteaucoeurdelaruche@gmail.com>
2026-02-10 13:49:13 +0100

    Update auth.js

--- LGA/auth.js
+++ LGA/auth.js
@@ -5,16 +5,7 @@ const users = {
 
 	"Thomas":"c498bfb76b6522f7593d129037cfcf763b470c620cc95062a1e21d6cd81415a8",
 	"Catherine":"e57d45172320c4147032444831a60383210a060f29da18d9cd1ac5f03cd197df"
-    // Format: "username": "SHA256_hash_du_mot_de_passe"
-    
-    // Exemple: utilisateur "apiculteur" avec mot de passe "ruche2024"
-    // Hash SHA256 de "ruche2024": ceffbe62205c7a6b59f7d54b2b38b35390d5047cd75a6a8e9aca9c453802c1fe
-    "apiculteur": "ceffbe62205c7a6b59f7d54b2b38b35390d5047cd75a6a8e9aca9c453802c1fe",
-    
-    // Pour ajouter un nouvel utilisateur, générez le hash SHA256 de votre mot de passe
-    // Vous pouvez utiliser ce site: https://emn178.github.io/online-tools/sha256.html
-    // Puis ajoutez la ligne: "nom_utilisateur": "hash_sha256_du_mot_de_passe"
-};
+ };
 
 // Fonction pour hasher avec SHA256
 async function sha256(message) {
