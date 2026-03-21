044f14db097e043929822e57ce0dc98e72900f24
frelon-la-comte <lacomteaucoeurdelaruche@gmail.com>
2026-03-21 20:53:06 +0100

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
 const GPS_MAPPING = {
     "centre": [50.4256, 2.4993], // Centre du village par défaut
     "rue de l'église": [50.4282, 2.4915],
-    "grand rue": [50.4275, 2.4985],
+    "grande rue": [50.4275, 2.4985],
     "rue jules elby": [50.4282, 2.4985],
     "rue du 14 juillet": [50.4270, 2.5039],
 	"rue de la petite ville": [50.4254, 2.4987],
