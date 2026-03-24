94d420d25855ce3a10c5c0e3bf6d27a49d8eb978
frelon-la-comte <lacomteaucoeurdelaruche@gmail.com>
2026-03-24 12:54:05 +0100

    Update data

--- dashboard.js
+++ dashboard.js
@@ -10,11 +10,11 @@ const GPS_MAPPING = {
     "grande rue": [50.4275, 2.4985],
     "rue jules elby": [50.4282, 2.4985],
     "rue du 14 juillet": [50.4270, 2.5039],
-	"rue de la petite ville": [50.4254, 2.4987],
-	"rue du 8 mai 1945": [50.4215, 2.5022],
-	"rue du chateau": [50.4242, 2.5010],
-	"rue du 19 mars 1962": [50.4244, 2.4963],
-	"rue du 11 novembre": [50.4255, 2.4937],
+    "rue de la petite ville": [50.4254, 2.4987],
+    "rue du 8 mai 1945": [50.4215, 2.5022],
+    "rue du chateau": [50.4242, 2.5010],
+    "rue du 19 mars 1962": [50.4244, 2.4963],
+    "rue du 11 novembre": [50.4255, 2.4937],
     
 };
 
